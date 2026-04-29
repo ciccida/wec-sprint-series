de0fdaffc592e8ce90ae5d4ca543f2be503b083f
千田健太 <kentachida@KCs-MacBook-Pro14inch.local>
2026-04-29 12:36:33 +0900

    Change PARTNER link color to default white

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,11 +38,7 @@ const Navbar = () => {
                     <li><Link to="/results">RESULTS</Link></li>
                     <li><Link to="/regulations">REGULATIONS</Link></li>
                     <li><Link to="/setup-engineer">AI SETUP</Link></li>
-                    <li>
-                        <Link to="/sponsorship" style={{ color: '#ff003c', fontWeight: 'bold' }}>
-                            PARTNER
-                        </Link>
-                    </li>
+                    <li><Link to="/sponsorship">PARTNER</Link></li>
                     <li>
                         <a href="/weather/" className="nav-tool-link">
                             WEATHER

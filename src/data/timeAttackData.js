976fe745c8d522d73597b0ecd4d1acbd6edc3d56
千田健太 <kentachida@KCs-MacBook-Pro14inch.local>
2026-05-07 07:56:02 +0900

    Add Masa Matsumura to time attack results

--- src/data/timeAttackData.js
+++ src/data/timeAttackData.js
@@ -9,6 +9,7 @@ export const timeAttackData = {
       image: "/images/ta_s3_rd1.jpg", 
       results: [
         { name: "Koki Yamamoto", class: "HYPERCAR", car: "Genesis GMR-001 LMDh", time: "1:35.445", attempt: 1 },
+        { name: "Masa Matsumura", class: "HYPERCAR", car: "Peugeot 9X8 2024", time: "1:36.263", attempt: 2 },
         { name: "Tomoya Onodera", class: "HYPERCAR", car: "Peugeot 9X8 2024", time: "1:36.305", attempt: 2 },
         { name: "kaeru uenchu", class: "HYPERCAR", car: "Porsche 963", time: "1:37.172", attempt: 1 },
         { name: "Sasuke_yuuuki", class: "HYPERCAR", car: "Toyota GR010 Hybrid", time: "1:37.311", attempt: 1 },

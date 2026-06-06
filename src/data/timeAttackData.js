34de58aeac9053101346b9027aebcacae4983dd2
千田健太 <kentachida@KCs-MacBook-Pro14inch.local>
2026-06-07 07:58:11 +0900

    feat: update Time Attack Vol3 Rd3 results from Google Sheets

--- src/data/timeAttackData.js
+++ src/data/timeAttackData.js
@@ -28,12 +28,18 @@ export const timeAttackData = {
       
       ] },
     "3": { image: "/images/ta/vol3_rd3_cover.jpg", results: [
+        { name: "ぱすてぃ", class: "HYPERCAR", car: "Peugeot 9X8", time: "1:43.125", attempt: 1 },
         { name: "Masa Matsumura", class: "HYPERCAR", car: "Peugeot 9X8", time: "1:43.340", attempt: 3 },
         { name: "Koki Yamamoto", class: "HYPERCAR", car: "GLICKENHAUS SCG 007", time: "1:43.656", attempt: 2 },
         { name: "Salios SON", class: "HYPERCAR", car: "GLICKENHAUS SCG 007", time: "1:46.553", attempt: 1 },
         { name: "Rapid Tuyopon", class: "LMGT3", car: "BMW M4 LMGT3 Evo", time: "1:59.940", attempt: 1 },
         { name: "Tomoya Onodera", class: "LMGT3", car: "McLaren 720S LMGT3 Evo", time: "2:00.206", attempt: 1 },
         { name: "Salios SON", class: "LMGT3", car: "Lexus RC F LMGT3", time: "2:00.528", attempt: 1 }
+      
+      
+      
+      
+      
       
       
       

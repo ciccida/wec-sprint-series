eba4e27de6af55ffd61f84b0443ba6fc6a3fc9ca
千田健太 <kentachida@KCs-MacBook-Pro14inch.local>
2026-06-21 16:47:41 +0900

    Restore missing Rd1 Time Attack results

--- src/data/timeAttackData.js
+++ src/data/timeAttackData.js
@@ -8,7 +8,16 @@ export const timeAttackData = {
     "1": {
       image: "/images/ta_s3_rd1.jpg", 
       results: [
-
+        { name: "Koki Yamamoto", class: "HYPERCAR", car: "Genesis GMR-001 LMDh", time: "1:35.445", attempt: 1 },
+        { name: "Masa Matsumura", class: "HYPERCAR", car: "Peugeot 9x8 EVO", time: "1:36.263", attempt: 2 },
+        { name: "Tomoya Onodera", class: "HYPERCAR", car: "Peugeot 9X8 (2024 / 2023)", time: "1:36.305", attempt: 2 },
+        { name: "Yasu Tanaka", class: "HYPERCAR", car: "Glickenhaus SCG 007", time: "1:37.027", attempt: 1 },
+        { name: "kaeru uenchu", class: "HYPERCAR", car: "Porsche 963", time: "1:37.172", attempt: 1 },
+        { name: "Yuki", class: "HYPERCAR", car: "Toyota GR010 Hybrid", time: "1.37:311", attempt: 1 },
+        { name: "むひむーひー", class: "LMGT3", car: "Corvette Z06", time: "1:49.138", attempt: 1 },
+        { name: "Rapid Tuyopon", class: "LMGT3", car: "BMW M4 LMGT3", time: "1:49.943", attempt: 1 },
+        { name: "KH-AE86KMS", class: "LMGT3", car: "Porsche 911 GT3 R (992)", time: "1:51.258", attempt: 2 },
+        { name: "チダ(ciccida)", class: "LMGT3", car: "Lexus RC F LMGT3", time: "1:51.387", attempt: 2 }
       ]
     },
     "2": { 
